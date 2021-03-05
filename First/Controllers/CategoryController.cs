d1ccb4f4a1615ece30a4bd10386d2c5968db0ff4
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-03-05 21:14:03 +0200

    Adding some controllers and views

---
+++ First/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using First.Data;
+using First.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+
+        // GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
