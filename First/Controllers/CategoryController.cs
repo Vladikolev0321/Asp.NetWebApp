d427646a04e9dd7d6c9b75de47867ccb31b49571
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-03-05 22:04:08 +0200

    Added validation for Category

--- First/Controllers/CategoryController.cs
+++ First/Controllers/CategoryController.cs
@@ -35,9 +35,13 @@ namespace First.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if(ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
--- First/Models/Category.cs
+++ First/Models/Category.cs
@@ -10,7 +10,10 @@ namespace First.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Display Order for category must be greater than 0")]
         public int DisplayOrder { get; set; }
 
     }
